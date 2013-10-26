db8fd944c0867ae9e45be91c78061a918d048ae4
Michael Napoleon <michael.napoleon@gmail.com>
2013-10-25 20:57:45 -0400

    renamed test, fixed test, fixed call back on appOpened function

--- test/analytics.test.js
+++ test/analytics.test.js
@@ -1,18 +1,23 @@
 var config = require('./config');
 var should = require('should');
-var async = require('async');
 var Kaiseki = require('../lib/kaiseki');
-var _ = require('underscore');
 
 var parse = new Kaiseki(config.PARSE_APP_ID, config.PARSE_REST_API_KEY);
 
 describe('analytics', function() {
-	it ('can update app open', function(done) {
-		parse.appOpened(null, function(err, res, body, success) {
+	var updatetime = {
+		at: {
+	    	__type: 'Date',
+	        iso: "2013-10-25T21:23:19Z"
+	     }
+	 };
+	it ('can update app opened', function(done) {
+		parse.appOpened(updatetime, function(err, res, body, success) {
 			success.should.be.true;
 			should.not.exist(err);
-
-			done(err);
+			should.exist(res);
+			should.exist(body);
+			done();
 		});
 	});	
 });
